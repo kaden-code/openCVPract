4d7cc4db848aa89b57f9fc481d546b4e838041d8
kaden-code <118853686+kaden-code@users.noreply.github.com>
2024-01-26 02:03:38 -0500

    added hsv color space window to left mouse over

--- hsvColorSpace.py
+++ hsvColorSpace.py
@@ -37,7 +37,16 @@ def mouseClick(event,xPos,yPos,flags,params):
 
 startInput = input(startQuestion)
 window = "window"
+
+bgrDisplayWindow = "bgrWindow"
+hsvDisplayWindow = "hsvWindow"
 black = (0,0,0)
+displaySquare = 250
+pixelEncoding = 3
+textCordinates = (0,50)
+font = cv2.FONT_HERSHEY_COMPLEX
+fontScale = 1
+fontThickness = 1
 
 if startInput == startKey:
     camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)
@@ -51,13 +60,24 @@ if startInput == startKey:
     while True:
       frame = returnFrame(camera)
       if clickType == 0:
-        x = np.zeros([250,250,3],dtype = np.uint8)
-        clickColor = frame[clickPosition[1]][clickPosition[0]]
-        print(clickColor)
-        x[:,:] = clickColor  
-        cv2.putText(x,str(clickColor),(0,50),cv2.FONT_HERSHEY_COMPLEX,1,black,1)
-        cv2.imshow("Color Picker", x)    
-        cv2.moveWindow("Color Picker", cameraWidth,0)
+        
+        bgrDisplayFrame = np.zeros([displaySquare,displaySquare,pixelEncoding],dtype = np.uint8)
+        hsvDisplayFrame = np.zeros([displaySquare,displaySquare,pixelEncoding],dtype = np.uint8)
+        ## converts this creates another matrix with same dimensions from bgr(blue,green,red) color sapce to hsv (hue saturation value) color space
+        hsvFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        clickColorBGR = frame[clickPosition[1]][clickPosition[0]]
+        clickColorHSV = hsvFrame[clickPosition[1]][clickPosition[0]]
+        print(clickColorBGR)
+        print(clickColorHSV)
+        bgrDisplayFrame[:,:] = clickColorBGR
+        hsvDisplayFrame[:,:] = clickColorHSV
+        hsvDisplayFrame = cv2.cvtColor(hsvDisplayFrame,cv2.COLOR_HSV2BGR)
+        cv2.putText(bgrDisplayFrame,str(clickColorBGR),textCordinates,font,fontScale,black,fontThickness)
+        cv2.imshow(bgrDisplayWindow, bgrDisplayFrame)    
+        cv2.putText(hsvDisplayFrame,str(clickColorHSV),textCordinates,font,fontScale,black,fontThickness)
+        cv2.moveWindow(bgrDisplayWindow, cameraWidth,0)
+        cv2.imshow(hsvDisplayWindow, hsvDisplayFrame)    
+        cv2.moveWindow(hsvDisplayWindow, cameraWidth,displaySquare)
         clickType = None     
       cv2.imshow(window,frame)
       if cv2.waitKey(1) == ord("q"):
