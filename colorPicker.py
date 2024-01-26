aa40d33f51f5d866967f9e74b86cbc1d59aed25c
kaden-code <118853686+kaden-code@users.noreply.github.com>
2024-01-26 09:24:43 -0500

    Added hsv color selection window with track bars

--- colorPicker.py
+++ colorPicker.py
@@ -5,24 +5,98 @@ startPass = "A!"
 displaySquare = 720
 userAnswer = None 
 pixelEncoding = 3
-blue = (255,0,0)
+bgrBlueValue = 0
+bgrGreenValue = 0
+bgrRedValue = 0
+
+hsvHueValue = 0
+hsvSaturationValue = 0
+hsvValueValue = 255
+
 
 
 bgrDisplayWindow = "bgr color"
+bgrTrackWindow = "Customize Blue Green Red"
+valueStart = 0
+bgrColorValueMax = 255
+hueMax = 179
+saturationMax = 255
+valueMax = 255
+hsvDisplayWindow = "hsv color"
+hsvTrackWindow = "Customize Hue Saturation Value"
+
+
+
+
+
+
+def changeBlueValueBGR(val):
+   global bgrBlueValue
+   bgrBlueValue = val
+   
+
+def changeRedValueBGR(val):
+   global bgrRedValue
+   bgrRedValue = val
+
+def changeGreenValueBGR(val):
+   global bgrGreenValue
+   bgrGreenValue = val
+
+
+
+def changeHSVHue(val):
+   global hsvHueValue
+   hsvHueValue = val
+
+def changeHSVSaturation(val):
+   global hsvSaturationValue
+   hsvSaturationValue = val
+
+def changeHSVValue(val):
+   global hsvValueValue
+   hsvValueValue = val
+   
+   
+   
+
+
+def makeColorBGR():
+    frame = np.zeros([displaySquare,displaySquare,pixelEncoding], dtype = np.uint8)
+   ## cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    bgrColor = (bgrBlueValue,bgrGreenValue,bgrRedValue)
+
+    frame[:,:] = bgrColor
+    return frame
+
 
 def makeColorHSV():
     frame = np.zeros([displaySquare,displaySquare,pixelEncoding], dtype = np.uint8)
-   ## cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    frame[:,:] = blue 
+    cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    hsvColor = (hsvHueValue,hsvSaturationValue,hsvValueValue)
+    
+    frame[:,:] = hsvColor
+    cv2.cvtColor(frame,cv2.COLOR_HSV2BGR)
     return frame
 
 userAnswer = input(startQuestion)
 if userAnswer == startPass:
     cv2.namedWindow(bgrDisplayWindow)
+    cv2.namedWindow(bgrTrackWindow)
+    cv2.namedWindow(hsvDisplayWindow)
+    cv2.namedWindow(hsvTrackWindow)
+    cv2.createTrackbar("Blue: ", bgrTrackWindow,valueStart,bgrColorValueMax, changeBlueValueBGR)
+    cv2.createTrackbar("Green: ", bgrTrackWindow,valueStart,bgrColorValueMax, changeGreenValueBGR)
+    cv2.createTrackbar("Red: ", bgrTrackWindow,valueStart,bgrColorValueMax, changeRedValueBGR)
 
+    cv2.createTrackbar("Hue: ", hsvTrackWindow,valueStart,hueMax, changeHSVHue)
+    cv2.createTrackbar("Saturation: ", hsvTrackWindow,valueStart,saturationMax, changeHSVSaturation)
+    cv2.createTrackbar("Value: ", hsvTrackWindow,valueStart,valueMax, changeHSVValue)  
     while True:
-      frame = makeColorHSV()
-      cv2.imshow(bgrDisplayWindow, frame)
+      bgrFrame = makeColorBGR()
+      cv2.imshow(bgrDisplayWindow, bgrFrame)
+      hsvFrame = makeColorHSV()
+      cv2.imshow(hsvDisplayWindow, hsvFrame)
 
-      if cv2.waitKey(1) == ord("k"):
+      if cv2.waitKey(1) == ord("q"):
          break
