66783a0d0b6e1f625efbf91ded3d12b1b7dba44c
kaden-code <118853686+kaden-code@users.noreply.github.com>
2024-02-10 02:37:55 -0500

    camera stream for face detection

--- faceDetection.py
+++ faceDetection.py
@@ -1,10 +1,39 @@
-import os
 import cv2
 
-# Get the base directory of the cv2 module
-cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
+startQuestion = "Enter Key: "
+startKey = "AI"
 
-# Construct the full path to the Haar cascade XML file
-haar_model_path = os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
+cameraWidth = 1280
+cameraHeight = 720
 
-print("Path to Haar cascade for face detection:", haar_model_path)
+def cameraSizing(width,height):
+     camera.set(cv2.CAP_PROP_FRAME_WIDTH,width)
+     camera.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
+
+
+def cameraFps(fps):
+     camera.set(cv2.CAP_PROP_FPS,fps)
+                           ## lets windows know that the video captures intent is to show allowing for faster performace 
+
+
+
+
+
+def returnFrame(camera):
+      ignore,frame = camera.read()
+      return frame
+
+startInput = input(startQuestion)
+
+if startInput == startKey:
+    camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+##formats how the video gets decoded (Set as moving jpeg)
+    camera.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*"MJPG"))
+    cameraSizing(cameraWidth,cameraHeight)
+    cameraFps(30)
+    
+    while True:
+      frame = returnFrame(camera)
+      cv2.imshow("Camera window",frame)
+      if cv2.waitKey(1) == ord("q"):
+            break
